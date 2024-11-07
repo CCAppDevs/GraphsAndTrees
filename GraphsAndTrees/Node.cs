b2c5cf6f0f387bfad4d5edf4b77b32afdcf24bef
Jesse Harlan <jesse.harlan@centralia.edu>
2024-11-07 13:52:12 -0800

    Add project files.

---
+++ GraphsAndTrees/Node.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphsAndTrees
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public static int CheckCount { get; set; }
+
+        public bool HasValue(T check)
+        {
+            CheckCount++;
+
+            // base case
+            if (check.CompareTo(Value) == 0)
+            { 
+                return true;
+            }
+
+            if (check.CompareTo(Value) < 0)
+            {
+                if (Left == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return Left.HasValue(check);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return Right.HasValue(check);
+                }
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (value.CompareTo(Value) < 0)
+            {
+                if (Left == null)
+                {
+                    Left = new Node<T>(value);
+                }
+                else
+                {
+                    Left.Add(value);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new Node<T>(value);
+                }
+                else
+                {
+                    Right.Add(value);
+                }
+            }
+        }
+    }
+}
---
+++ GraphsAndTrees/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphsAndTrees
+{
+    public class Person : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null) return 1;
+            if (obj == this) return 0;
+
+            if ((obj as Person).Birthdate == Birthdate)
+            {
+                return 0;
+            }
+            else if ((obj as Person).Birthdate < Birthdate)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+            
+        }
+    }
+}
